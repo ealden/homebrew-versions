4ea26c86fc4fa0ed38a8dbab381666afb08a63e3
Ealden Esto E. Escanan <ealden@teamcodeflux.com>
2012-05-16 14:08:42 +0800

    Grails 1.3.8
    This formula is actually a replacement for the older grails137.rb, as newer versions of Grails 1.3.x is released by SpringSource.
    
    I'm not sure if we can proceed and remove grails137.rb already.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@teamcodeflux.com>

---
+++ grails13.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Grails13 < Formula
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.8.zip'
+  homepage 'http://grails.org'
+  md5 'd28ac73ae160ce512f7f0c957f895995'
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install %w[LICENSE README]
+    libexec.install Dir['*']
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |f|
+      next unless File.extname(f).empty?
+      ln_s f, bin+File.basename(f)
+    end
+  end
+end
